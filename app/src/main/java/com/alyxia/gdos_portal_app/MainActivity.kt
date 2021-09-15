f9da8f3a272a2776fe0b3b2acb03ef49bd52d550
Alyxia Sother <lexisoth2005@gmail.com>
2021-09-15 13:42:01 +0200

    [Main] Added header text

--- app/src/main/java/com/alyxia/gdos_portal_app/MainActivity.kt
+++ app/src/main/java/com/alyxia/gdos_portal_app/MainActivity.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alyxia.gdos_portal_app.ui.theme.GDOSPortalAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +24,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Column {
+                        Text(modifier = Modifier.padding(top = 100.dp).align(Alignment.CenterHorizontally), fontSize = 25.sp, text = "Login to")
+                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), fontSize = 25.sp, text = "Gilde DevOps Portal")
+                    }
                     Column(
                         modifier = Modifier
                             .wrapContentHeight()
@@ -37,14 +40,19 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.align(Alignment.CenterHorizontally),
                             label = { Text("Email") },
                             value = emailState.value,
-                            onValueChange = { emailState.value = it })
+                            onValueChange = { emailState.value = it }
+                        )
                         TextField(
                             modifier = Modifier.align(Alignment.CenterHorizontally),
                             label = { Text("Password") },
                             value = passState.value,
-                            onValueChange = { passState.value = it })
-                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "The email field has this text: ${emailState.value.text}")
-                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "The password field has this text: ${passState.value.text}")
+                            onValueChange = { passState.value = it }
+                        )
+                        Button(modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 10.dp), onClick = { /* TODO */ }) {
+                            Text("Login")
+                        }
+//                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "The email field has this text: ${emailState.value.text}")
+//                        Text(modifier = Modifier.align(Alignment.CenterHorizontally), text = "The password field has this text: ${passState.value.text}")
                     }
                 }
             }
